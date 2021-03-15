2a0d1b87ff2424613cd665b705854671023f6dee
mission-deny-the-mission <mission.deny.the.mission@gmail.com>
2021-03-15 02:08:53 +0100

    remove unused files

--- symptoms.pl
+++
@@ -1,3 +0,0 @@
-symptomsubtype('rash', 'facial butterfly rash').
-
-symptomdescription('Raynaud\'s phenomenon', 'Fingers and toes that turn white or blue when exposed to cold or during stressful periods').
